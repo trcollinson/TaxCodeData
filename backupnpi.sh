62da195bac3b97f4469510332164b6c894994635
Tim Collinson <trcollinson@gmail.com>
2015-08-07 15:20:50 -0600

    Updated backup script to work a little smart.

--- backupnpi.sh
+++ backupnpi.sh
@@ -1,3 +1,7 @@
 #! /bin/bash
 
+if [ -e backup/npi-data-$(date +%F).sql ]; then
+	tar -czvf backup/npi-data-$(date +%F-%H%M%S).tar.gz backup/npi-data-$(date +%F).sql
+fi
+
 mysqldump --user=npi-user --password npi_data > backup/npi-data-$(date +%F).sql
